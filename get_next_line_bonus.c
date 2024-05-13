3531a19ab3a931c0a556950303e6e8cd7823551e
Rui Miguel Soares Martins <rusoares@c1r3s1.42porto.com>
2024-05-13 13:55:20 +0100

    bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,31 @@
 /*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 13:13:48 by rusoares          #+#    #+#             */
-/*   Updated: 2024/05/13 13:13:49 by rusoares         ###   ########.fr       */
+/*   Updated: 2024/05/13 13:45:59 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line_bonus.h"
+
+char	*get_next_line(int fd)
+{
+	static char     *bag;
+	char			*line;
+	char			*tmp;
+	int				bytes;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	tmp = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!tmp)
+		return (NULL);
+	bytes = read(fd, bag,BUFFER_SIZE);
+	if (bytes == -1)
+	{
+		free (tmp);
+		return (NULL);
+	}
+	tmp[bytes] = '\0';
+	
+	
+}
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,23 @@
 /*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 13:13:54 by rusoares          #+#    #+#             */
-/*   Updated: 2024/05/13 13:13:55 by rusoares         ###   ########.fr       */
+/*   Updated: 2024/05/13 13:29:33 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+/*Librarias*/
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+/*Funcoes*/
+char	*get_next_line(int fd);
+
+#endif
