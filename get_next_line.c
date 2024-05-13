98a60f5308cd65ee155851c16c288ef60e827b40
Rui Miguel Soares Martins <rusoares@c1r3s1.42porto.com>
2024-05-13 13:14:18 +0100

    updated version

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,101 @@
 /*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 10:17:01 by rusoares          #+#    #+#             */
-/*   Updated: 2024/05/09 11:14:29 by rusoares         ###   ########.fr       */
+/*   Updated: 2024/05/13 10:52:26 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "get_next_line.h"
+#include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
+	static char	*left;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	left = read_function (fd, left);
+	if (!left)
+		return (NULL);
+	line = ft_getline(left);
+	left = ft_getleft(left);
+	return (line);
+}
+
+char	*read_function(int fd, char *str)
+{
+	char	*tmp;
+	int		bytes;
+
+	tmp = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!tmp)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(str, '\n') && (bytes != 0))
+	{
+		bytes = read(fd, tmp, BUFFER_SIZE);
+		if (bytes == -1)
+		{
+			free(tmp);
+			return (NULL);
+		}
+		tmp[bytes] = '\0';
+		str = ft_strjoin(str, tmp);
+	}
+	free(tmp);
+	return (str);
+}
+
+char	*ft_getline(char *left)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!left[i])
+		return (NULL);
+	while (left[i] && left[i] != '\n')
+		i++;
+	line = (char *)malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (left[i] && left[i] != '\n')
+	{
+		line[i] = left[i];
+		i++;
+	}
+	if (left[i] == '\n')
+	{
+		line[i] = left[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_getleft(char *left)
+{
+	char	*rest;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (left[i] && left[i] != '\n')
+		i++;
+	if (!left[i])
+	{
+		free(left);
+		return (NULL);
+	}
+	rest = (char *)malloc(sizeof(char) * (ft_strlen(left) - i + 1));
+	if (!rest)
+		return (NULL);
+	i++;
+	j = 0;
+	while (left[i])
+		rest[j++] = left[i++];
+	rest[j] = '\0';
+	free(left);
+	return (rest);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,29 @@
 /*   By: rusoares <rusoares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 10:17:47 by rusoares          #+#    #+#             */
-/*   Updated: 2024/05/09 10:24:38 by rusoares         ###   ########.fr       */
+/*   Updated: 2024/05/13 13:08:14 by rusoares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# ifndef BUFFER_size
+# ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
 /*Librarias*/
 # include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
 
 /*Funcoes*/
 char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *str1, char *str2);
+char	*read_function(int fd, char *str);
+char	*ft_getline(char *left);
+char	*ft_getleft(char *left);
 
 #endif
